2bbedf1919d39230e4a222cc9df47be4109273d9
ReynanSantts <reynanmesquita016@gmail.com>
2025-07-22 11:10:54 -0300

    feat: Pagina de registrar finalizada

--- register.php
+++ register.php
@@ -1,2 +1,31 @@
 <?php
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="paginaRegister.css">
+    <title>Página de registro</title>
+</head>
+<body>
+    <div class="container">
+        <div class="logo-area">
+            <img src="images/logo NextShop.png" alt="NextShop Logo" class="logo-img">
+        </div>
+        <div class="left-area">
+            <p class="slogan">
+                NextShop,<br>
+                conectando<br>
+                negócios e pessoas
+            </p>
+            <span class="highlight">o próximo é você</span>
+        </div>
+        <div class="buttons-area">
+            <button class="btn-login">JÁ TENHO CONTA</button>
+            <button class="btn-registrar" onclick="window.location.href='registerInfo.php'">CRIAR CONTA</button>
+        </div>
+    </div>
+</body>
+</html>
