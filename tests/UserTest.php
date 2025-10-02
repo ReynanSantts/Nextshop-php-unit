8bf413237f8ec6905514edd5cfa5ef8b2b583f72
Chaves777 <lc102434@gmail.com>
2025-10-02 08:32:12 -0300

    test: Complementando teste de usuario

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -49,4 +49,77 @@ class UserTest extends TestCase
         $this->assertEquals('reynanmesquita@gmail.com', $_SESSION['user_email']);
         $this->assertEquals('reynanmesquita@gmail.com', $_SESSION['email']);
     }
+
+    #[\PHPUnit\Framework\Attributes\Test]
+    public function it_shouldnt_login_with_invalid_credentials()
+    {
+        $this->mockUserModel->method('getUserByEmail')->willReturn([
+            "id" => 1,
+            "user_name" => "Reynan Mesquita",
+            "user_email" => "reynanmesquita@gmail.com",
+            "password" => password_hash("123456", PASSWORD_DEFAULT)
+        ]);
+
+        $userResult = $this->userController->login('reynanmesquita@gmail.com', 'senha_errada');
+
+        $this->assertFalse($userResult);
+    }
+
+    #[\PHPUnit\Framework\Attributes\Test]
+    public function it_should_be_able_to_check_user_by_email()
+    {
+        $this->mockUserModel->method('getUserByEmail')->willReturn([
+            "id" => 1,
+            "user_name" => "Reynan Mesquita",
+            "user_email" => "reynanmesquita@gmail.com",
+            "password" => password_hash('123456', PASSWORD_DEFAULT)
+        ]);
+
+        $userResult = $this->userController->checkUserByEmail('reynanmesquita@gmail.com');
+
+        $this->assertNotNull($userResult);
+        $this->assertEquals('reynanmesquita@gmail.com', $userResult['user_email']);
+    }
+
+    #[\PHPUnit\Framework\Attributes\Test]
+    public function it_should_verify_if_is_logged_in()
+    {
+        $_SESSION['id'] = 1;
+
+        $userResult = $this->userController->isLoggedIn();
+
+        $this->assertTrue($userResult);
+    }
+
+    #[\PHPUnit\Framework\Attributes\Test]
+    public function it_should_not_create_user_with_empty_fields()
+    {
+        $result = $this->userController->createUser('', 'reynanmesquita@gmail.com', '123.456.789-00', '123456');
+        $this->assertFalse($result);
+
+        $result = $this->userController->createUser('Reynan Mesquita', '', '123.456.789-00', '123456');
+        $this->assertFalse($result);
+
+        $result = $this->userController->createUser('Reynan Mesquita', 'reynanmesquita@gmail.com', '', '123456');
+        $this->assertFalse($result);
+
+        $result = $this->userController->createUser('Reynan Mesquita', 'reynanmesquita@gmail.com', '123.456.789-00', '');
+        $this->assertFalse($result);
+    }
+
+    #[\PHPUnit\Framework\Attributes\Test]
+    public function it_should_get_first_user()
+    {
+        $userData = [
+            "id" => 1,
+            "user_name" => "Reynan Mesquita",
+            "user_email" => "reynanmesquita@gmail.com"
+        ];
+
+        $this->mockUserModel->method('getFirstUser')->willReturn($userData);
+
+        $result = $this->userController->getFirstUser();
+        $this->assertEquals($userData, $result);
+    }
 }
+
